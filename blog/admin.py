3fe20914bb2b688ab28af44f711732100ca3c225
saphirbleu <gradikenga1@gmail.com>
2026-03-12 17:56:12 +0100

    Modification du modèle et insertion des données depuis l'interface administrateur

--- blog/admin.py
+++ blog/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from . models import *
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['title', 'auth', 'contain', 'date_of_publish', 'statut', 'category']
+    list_display = ('title', 'author', 'category', 'date_of_publish', 'statut', 'content')
+    ordering = ['-date_of_publish']
 
 # Register your models here.
 admin.site.register(Categorie)
--- blog/models.py
+++ blog/models.py
@@ -4,17 +4,29 @@ from django.contrib.auth.models import User
 
 # Initialisation de la class Categorie
 class Categorie(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name="Non de la catégorie")
     
     def __str__(self):
         return self.name
     
+    class Meta:
+        verbose_name = "Catégorie"
+        verbose_name_plural = "Catégories"
+        
+            
 # Initialisation de la class Article
 class Article(models.Model):
-    title = models.CharField(max_length=255)
-    auth = models.ForeignKey(User, on_delete=models.CASCADE)
-    contain = models.TextField(max_length=400)
-    date_of_publish = models.DateField(auto_now_add=True)
-    statut = models.BooleanField()
-    category = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name="Titre")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ar", verbose_name="Auteur")
+    content = models.TextField(verbose_name="Contenu")
+    date_of_publish = models.DateTimeField(auto_now_add=True, verbose_name="Date de publication")
+    statut = models.BooleanField(default=False, verbose_name="Publié")
+    category = models.ForeignKey(Categorie, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Categorie")
     
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+        
